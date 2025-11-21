bad0e97587f8f4643978fe8605e3f21dfccdf987
Lucascla18 <lucascla18@gmail.com>
2025-11-20 22:56:43 -0300

    Adicionei o APK

--- www/chunk-G7T7QN72.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-7KGURMOZ.js";import"./chunk-BDMMXEV4.js";export{a as startFocusVisible};
--- www/chunk-Y34F2D7Q.js
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-MDMZPGPE.js";import"./chunk-BDMMXEV4.js";export{a as GESTURE_CONTROLLER,b as createGesture};
